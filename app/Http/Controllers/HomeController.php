ab6d0662c805f82e92091a2329f9d25a60f5afbe
nblkmal <nblkmal@gmail.com>
2021-08-08 23:04:46 +0800

    add notify in controller ->notify(new < noti model >)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Notifications\TrainingNotification;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        auth()->user()->notify(new TrainingNotification);
+
         return view('home');
     }
 }
